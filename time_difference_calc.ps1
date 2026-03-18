54631130d30bea064eca3599f751ece734f08a22
Steven Wells <skordion@hotmail.com>
2026-03-18 09:41:42 -0400

    organize

--- time_difference_calc.ps1
+++ time_difference_calc.ps1
@@ -2,4 +2,4 @@ cd $PSScriptRoot
 git pull -q
 .venv/Scripts/Activate.ps1
 pip install -q -r requirements.txt
-python3 time_difference_calc_script.py @args
+python3 python_scripts/time_difference_calc_script.py @args
